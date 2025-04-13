63d5a3de1f6571b48f863446335660536017ec4b
viniciusbg18 <viniciusbaganhagodoy@gmail.com>
2025-04-12 22:43:13 -0300

    Create Comandos de strings importantes.py

---
+++ Comandos de strings importantes.py
@@ -0,0 +1,30 @@
+print('Comprimento da string:', len("inatel"))
+
+print("Pi:", str(3.1415))
+
+print("Caixa baixa:", "INATEL".lower())
+
+string = "inatel"
+print("Caixa alta:", string.upper())
+
+print("A string contém apenas letras?", "ws34rt".isalpha())
+
+print("A string contém apenas números?", "7000".isdigit())
+
+print("   remove todos espaços em branco.    ".strip())
+
+print("INATEL".replace('TEL','teste'))
+
+print("Olá, Mundo! 1, 2, 3".split(","))
+
+
+str1 = "Olá, Mundo!"
+fatia = str1[0:4]
+
+print(fatia)
+
+str1 = 'Eu'
+str2 = ' te '
+str3 = 'amooooo'
+str4 = str1 + str2 + str3
+print(str4)
